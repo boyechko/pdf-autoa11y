2ce86d36d5a3649fcece2579239725ea0ba53dca
Richard Boyechko <code@diachronic.net>
2025-09-08 16:49:14 -0700

    feature: Add support for password-protected PDFs

--- src/main/java/net/boyechko/pdf/preprocess/PdfNormalizerApp.java
+++ src/main/java/net/boyechko/pdf/preprocess/PdfNormalizerApp.java
@@ -7,21 +7,26 @@ import java.nio.file.*;
 public class PdfNormalizerApp {
     public static void main(String[] args) throws IOException {
         if (args.length == 0) {
-            System.err.println("Usage: java PdfNormalizerApp <filename>");
-            System.err.println("Example: java PdfNormalizerApp input.pdf");
+            System.err.println("Usage: java PdfNormalizerApp <filename> [password]");
+            System.err.println("Example: java PdfNormalizerApp input.pdf abracadabra");
             System.exit(1);
         }
 
         String file = args[0];
         String src  = "inputs/" + file;
         String dest = "outputs/" + file;
-
-        // Ensure output dir exists
         Files.createDirectories(Paths.get(dest).getParent());
 
-        // Normal, safe stamping: read src, write dest (different files)
-        try (PdfDocument pdf = new PdfDocument(new PdfReader(src), new PdfWriter(dest))) {
-            PdfTagNormalizer processor = new PdfTagNormalizer(pdf);
+        ReaderProperties readerProps = new ReaderProperties();
+        if (args.length == 2) {
+            String password = args[1];
+            readerProps.setPassword(password.getBytes());
+        }
+        PdfReader pdfReader = new PdfReader(src, readerProps);
+
+        try (PdfDocument pdfDoc = new PdfDocument(pdfReader, new PdfWriter(dest))) {
+            System.out.println("Number of pages: " + pdfDoc.getNumberOfPages());
+            PdfTagNormalizer processor = new PdfTagNormalizer(pdfDoc);
             processor.processAndDisplayChanges();
         }
     }
