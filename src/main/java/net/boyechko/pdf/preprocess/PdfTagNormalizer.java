625523b13644725a379dcb1ede6a4727ef5fb292
Richard Boyechko <code@diachronic.net>
2025-09-08 15:55:59 -0700

    refactor: Remove obsolete code

--- src/main/java/net/boyechko/pdf/preprocess/PdfTagNormalizer.java
+++ src/main/java/net/boyechko/pdf/preprocess/PdfTagNormalizer.java
@@ -41,15 +41,7 @@ public class PdfTagNormalizer {
         String indent = "  ".repeat(level);
         String role = elem.getRole().getValue();
 
-        if ("L".equals(role)) {
-            if (hasDesiredListStructure(elem)) {
-                System.out.println(indent + "- " + role + " 🎯 [Desired list structure found]");
-            } else {
-                System.out.println(indent + "- " + role);
-            }
-        } else {
             System.out.println(indent + "- " + role);
-        }
 
         for (Object kid : elem.getKids()) {
             if (kid instanceof PdfStructElem) {
@@ -83,80 +75,6 @@ public class PdfTagNormalizer {
             }
         }
         return false;
-    }
-
-    public void normalizeListStructures() {
-        if (root != null) {
-            System.out.println("Normalizing list structures in document...");
-            for (Object kid : root.getKids()) {
-                if (kid instanceof PdfStructElem) {
-                    processListStructures((PdfStructElem) kid);
-                }
-            }
-        } else {
-            System.out.println("No tag structure found in the document.");
-        }
-    }
-    
-    private void processListStructures(PdfStructElem elem) {
-        String role = elem.getRole().getValue();
-
-        if ("L".equals(role) && hasDesiredListStructure(elem)) {
-            convertPToLbl(elem);
-        }
-
-        for (Object kid : elem.getKids()) {
-            if (kid instanceof PdfStructElem) {
-                processListStructures((PdfStructElem) kid);
-            }
-        }
-    }
-
-    private void convertPToLbl(PdfStructElem listElem) {
-        for (Object kid : listElem.getKids()) {
-            if (kid instanceof PdfStructElem) {
-                PdfStructElem listItem = (PdfStructElem) kid;
-                if ("LI".equals(listItem.getRole().getValue())) {
-                    Object[] liKids = listItem.getKids().toArray();
-                    if (liKids.length == 2 && 
-                        liKids[0] instanceof PdfStructElem) {
-                        PdfStructElem pElem = (PdfStructElem) liKids[0];
-                        if ("P".equals(pElem.getRole().getValue())) {
-                            pElem.setRole(new PdfName("Lbl"));
-                            System.out.println("Converted P to Lbl in list item");
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    public void demoteH1Tags() {
-        if (root != null) {
-            System.out.println("Demoting H1 tags to H2 in document...");
-            for (Object kid : root.getKids()) {
-                if (kid instanceof PdfStructElem) {
-                    processAndDemoteH1((PdfStructElem) kid);
-                }
-            }
-        } else {
-            System.out.println("No tag structure found in the document.");
-        }
-    }
-
-    private void processAndDemoteH1(PdfStructElem elem) {
-        // Check the role of the current element
-        if ("H1".equals(elem.getRole().getValue())) {
-            elem.setRole(new PdfName("H2"));
-            System.out.println("Demoted H1 to H2.");
-        }
-
-        // Recursively process the children
-        for (Object kid : elem.getKids()) {
-            if (kid instanceof PdfStructElem) {
-                processAndDemoteH1((PdfStructElem) kid);
-            }
-        }
     }
 
     public void processAndDisplayChanges() {
