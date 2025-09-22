55a242b56489cdf45f444a3e4be2d83075f86b9f
Richard Boyechko <code@diachronic.net>
2025-09-22 11:51:04 -0700

    refactor(WrapInProperContainer): Rewrite as abstract class

--- src/main/java/net/boyechko/pdf/autoa11y/fixes/WrapInProperContainer.java
+++ src/main/java/net/boyechko/pdf/autoa11y/fixes/WrapInProperContainer.java
@@ -1,6 +1,5 @@
 package net.boyechko.pdf.autoa11y.fixes;
 
-import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfName;
 import com.itextpdf.kernel.pdf.tagging.PdfStructElem;
 import net.boyechko.pdf.autoa11y.DocumentContext;
@@ -8,13 +7,15 @@ import net.boyechko.pdf.autoa11y.IssueFix;
 
 import java.util.Optional;
 
-public final class WrapInProperContainer implements IssueFix {
-    private final PdfStructElem kid;
-    private final PdfStructElem parent;
-    private final String parentRole;
-    private final String kidRole;
+public abstract sealed class WrapInProperContainer implements IssueFix
+    permits WrapInProperContainer.WrapPInLILBody, WrapInProperContainer.WrapSpanInLBody {
 
-    private WrapInProperContainer(PdfStructElem kid, PdfStructElem parent, String parentRole, String kidRole) {
+    protected final PdfStructElem kid;
+    protected final PdfStructElem parent;
+    protected final String parentRole;
+    protected final String kidRole;
+
+    protected WrapInProperContainer(PdfStructElem kid, PdfStructElem parent, String parentRole, String kidRole) {
         this.kid = kid;
         this.parent = parent;
         this.parentRole = parentRole;
@@ -22,8 +23,10 @@ public final class WrapInProperContainer implements IssueFix {
     }
 
     public static Optional<IssueFix> createIfApplicable(PdfStructElem kid, PdfStructElem parent, String parentRole, String kidRole) {
-        if (("L".equals(parentRole) && "P".equals(kidRole)) || ("LI".equals(parentRole) && "Span".equals(kidRole))) {
-            return Optional.of(new WrapInProperContainer(kid, parent, parentRole, kidRole));
+        if ("L".equals(parentRole) && "P".equals(kidRole)) {
+            return Optional.of(new WrapPInLILBody(kid, parent, parentRole, kidRole));
+        } else if ("LI".equals(parentRole) && "Span".equals(kidRole)) {
+            return Optional.of(new WrapSpanInLBody(kid, parent, parentRole, kidRole));
         }
         return Optional.empty();
     }
@@ -33,43 +36,53 @@ public final class WrapInProperContainer implements IssueFix {
         return 30; // Content fix
     }
 
-    @Override
-    public void apply(DocumentContext ctx) throws Exception {
-        if ("L".equals(parentRole) && "P".equals(kidRole)) {
-            // Wrap P in LI->LBody
-            wrapPInLILBody(ctx.doc());
-        } else if ("LI".equals(parentRole) && "Span".equals(kidRole)) {
-            // Wrap Span in LBody
-            wrapSpanInLBody(ctx.doc());
-        }
-    }
-
-    @Override
-    public String describe() {
-        return "Wrapped " + kidRole + " in proper container under " + parentRole;
-    }
-
-    private void wrapPInLILBody(PdfDocument document) throws Exception {
-        PdfStructElem newLI = new PdfStructElem(document, PdfName.LI);
-        parent.addKid(newLI);
-
-        PdfStructElem newLBody = new PdfStructElem(document, PdfName.LBody);
-        newLI.addKid(newLBody);
-
-        parent.removeKid(kid);
-        newLBody.addKid(kid);
-    }
-
-    private void wrapSpanInLBody(PdfDocument document) throws Exception {
-        PdfStructElem newLBody = new PdfStructElem(document, PdfName.LBody);
-        parent.addKid(newLBody);
-
-        parent.removeKid(kid);
-        newLBody.addKid(kid);
-    }
-
     // Getters for invalidation logic in other fixes
     public PdfStructElem getKid() { return kid; }
     public PdfStructElem getParent() { return parent; }
     public String getParentRole() { return parentRole; }
+
+    public static final class WrapPInLILBody extends WrapInProperContainer {
+        private WrapPInLILBody(PdfStructElem kid, PdfStructElem parent, String parentRole, String kidRole) {
+            super(kid, parent, parentRole, kidRole);
+        }
+
+        @Override
+        public void apply(DocumentContext ctx) throws Exception {
+            PdfStructElem newLI = new PdfStructElem(ctx.doc(), PdfName.LI);
+            parent.addKid(newLI);
+
+            PdfStructElem newLBody = new PdfStructElem(ctx.doc(), PdfName.LBody);
+            newLI.addKid(newLBody);
+
+            parent.removeKid(kid);
+            newLBody.addKid(kid);
+        }
+
+        @Override
+        public String describe() {
+            return "Wrapped P element in LI->LBody container under L object #"
+                   + parent.getPdfObject().getIndirectReference().getObjNumber();
+        }
+    }
+
+    public static final class WrapSpanInLBody extends WrapInProperContainer {
+        private WrapSpanInLBody(PdfStructElem kid, PdfStructElem parent, String parentRole, String kidRole) {
+            super(kid, parent, parentRole, kidRole);
+        }
+
+        @Override
+        public void apply(DocumentContext ctx) throws Exception {
+            PdfStructElem newLBody = new PdfStructElem(ctx.doc(), PdfName.LBody);
+            parent.addKid(newLBody);
+
+            parent.removeKid(kid);
+            newLBody.addKid(kid);
+        }
+
+        @Override
+        public String describe() {
+            return "Wrapped Span element in LBody container under LI object #"
+                   + parent.getPdfObject().getIndirectReference().getObjNumber();
+        }
+    }
 }
