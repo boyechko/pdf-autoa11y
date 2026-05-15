c9cbc97d56482101be68b02cd6fdd79f7d568af6
Richard Boyechko <code@boyechko.net>
2026-05-15 10:06:37 -0700

    chore: Fix copyright date in files created in 2026

--- src/main/java/net/boyechko/pdf/autoa11y/checks/TabOrderCheck.java
+++ src/main/java/net/boyechko/pdf/autoa11y/checks/TabOrderCheck.java
@@ -1,6 +1,6 @@
 /*
  * PDF-Auto-A11y - Automated PDF Accessibility Remediation
- * Copyright (C) 2025 Richard Boyechko
+ * Copyright (C) 2026 Richard Boyechko
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
